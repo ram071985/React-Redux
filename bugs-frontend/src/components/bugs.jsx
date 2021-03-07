aaa4e119aca25b1ef9197d26901bf0b635fc1348
ram071985 <reidmuchow@gmail.com>
2021-03-06 19:16:09 -0600

    added bugs component and reference in app.js.

---
+++ bugs-frontend/src/components/bugs.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Bugs extends Component {
+    render() {
+        return (
+            <div>
+                Bugs
+            </div>
+        )
+    }
+}
